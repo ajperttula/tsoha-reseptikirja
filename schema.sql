d31da40bbb47edb7d3c52864c16b32873c11802b
Antti Perttula <anttiperttula@outlook.com>
2021-03-29 15:24:13 +0300

    Add sql schema

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role INTEGER
+);
+
+CREATE TABLE recipes (
+    id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users,
+    title TEXT,
+    description TEXT,
+    instruction TEXT
+);
+
+CREATE TABLE ingredients (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    ingredient TEXT
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    tag TEXT
+);
+
+CREATE TABLE recipetags (
+    recipe_id INTEGER REFERENCES recipes,
+    tag_id INTEGER REFERENCES tags
+);
