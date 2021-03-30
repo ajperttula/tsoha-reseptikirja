051049238c6b8b4d6c14b850603b0c835a1bff2d
Antti Perttula <anttiperttula@outlook.com>
2021-03-30 10:52:33 +0300

    Update SQL schema

--- schema.sql
+++ schema.sql
@@ -28,3 +28,16 @@ CREATE TABLE recipetags (
     recipe_id INTEGER REFERENCES recipes,
     tag_id INTEGER REFERENCES tags
 );
+
+CREATE TABLE commments (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    sender_id INTEGER REFERENCES users,
+    comment TEXT
+);
+
+CREATE TABLE grades (
+    id SERIAL PRIMARY KEY,
+    recipe_id INTEGER REFERENCES recipes,
+    grade INTEGER
+);
