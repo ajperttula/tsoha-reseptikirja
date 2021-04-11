fffc71c121c3e81172445f48463479105ed07c04
Antti Perttula <anttiperttula@outlook.com>
2021-04-11 21:46:02 +0300

    Add ability to modify own recipes

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 
 @app.route("/new-recipe")
 def new_recipe():
-    sql = "SELECT tag FROM tags"
+    sql = "SELECT id, tag FROM tags"
     tags = db.session.execute(sql).fetchall()
     return render_template("new-recipe.html", tags=tags)
 
@@ -28,18 +28,8 @@ def add_recipe():
     description = request.form["description"]
     instruction = request.form["instruction"]
     ingredients = request.form.getlist("ingredient")
-    if len(title) == 0:
-        return render_template("error.html", error="Reseptillä ei ole otsikkoa.")
-    if len(title) > 50:
-        return render_template("error.html", error="Otsikon maksimipituus on 50 merkkiä.")
-    if len(description) > 200:
-        return render_template("error.html", error="Kuvauksen maksimipituus on 200 merkkiä.")
-    if len(instruction) > 2000:
-        return render_template("error.html", error="Ohjeen maksimipituus on 2000 merkkiä.")
-    for i in ingredients:
-        if len(i) > 100:
-            return render_template("error.html", error="Ainesosan maksimipituus on 100 merkkiä.")
     tags = request.form.getlist("tag")
+    check_recipe_inputs(title, description, instruction, ingredients)
     creator_id = get_user_id()
     sql = """INSERT INTO recipes (creator_id, created_at, title, description, instruction, visible) 
              VALUES (:creator_id, NOW(), :title, :description, :instruction, 1) RETURNING id"""
@@ -50,12 +40,112 @@ def add_recipe():
             db.session.execute(sql, {"recipe_id":recipe_id, "i":i})
     for t in tags:
         if t != "":
-            sql = "SELECT id FROM tags WHERE tag=:t"
-            tag_id = db.session.execute(sql, {"t":t}).fetchone()[0]
-            sql = "INSERT INTO recipetags (recipe_id, tag_id, visible) VALUES (:recipe_id, :tag_id, 1)"
-            db.session.execute(sql, {"recipe_id":recipe_id, "tag_id":tag_id})
+            sql = "INSERT INTO recipetags (recipe_id, tag_id, visible) VALUES (:recipe_id, :t, 1)"
+            db.session.execute(sql, {"recipe_id":recipe_id, "t":t})
     db.session.commit()
     return redirect(f"/recipe/{recipe_id}")
+
+@app.route("/modify-recipe", methods=["POST"])
+def modify_recipe():
+    csrf_check(request.form["csrf_token"])
+    title = request.form["title"]
+    recipe_id = request.form["recipe_id"]
+    description = request.form["description"]
+    instruction = request.form["instruction"]
+    ingredients = request.form.getlist("ingredient")
+    recipetags = request.form.getlist("recipetag")
+    sql = "SELECT id, tag FROM tags"
+    tags = db.session.execute(sql).fetchall()
+    return render_template("modify-recipe.html", recipe_id=recipe_id, title=title, description=description, ingredients=ingredients, instruction=instruction, tags=tags, recipetags=recipetags)
+
+@app.route("/execute-modification", methods=["POST"])
+def execute_modification():
+    csrf_check(request.form["csrf_token"])
+    recipe_id = request.form["recipe_id"]
+    title = request.form["title"]
+    description = request.form["description"]
+    instruction = request.form["instruction"]
+    ingredients = request.form.getlist("ingredient")
+    tags = request.form.getlist("tag")
+    check_recipe_inputs(title, description, instruction, ingredients)
+
+    sql = "UPDATE recipes SET title=:title, description=:description, instruction=:instruction WHERE id=:recipe_id"
+    db.session.execute(sql, {"title":title, "description":description, "instruction":instruction, "recipe_id":recipe_id})
+
+    sql = "SELECT id FROM ingredients WHERE recipe_id=:recipe_id AND visible=1"
+    old_ingredients = db.session.execute(sql, {"recipe_id":recipe_id}).fetchall()
+    new_ingredients = [i for i in ingredients if i != ""]
+    for i in range(min(len(old_ingredients), len(new_ingredients))):
+        id = old_ingredients.pop(0)[0]
+        ingredient = new_ingredients.pop(0)
+        sql = "UPDATE ingredients SET ingredient=:ingredient WHERE id=:id"
+        db.session.execute(sql, {"ingredient":ingredient, "id":id})
+    if old_ingredients:
+        for i in old_ingredients:
+            id = i[0]
+            sql = "UPDATE ingredients SET visible=0 WHERE id=:id"
+            db.session.execute(sql, {"id":id})
+    else:
+        for i in new_ingredients:
+            sql = "INSERT INTO ingredients (recipe_id, ingredient, visible) VALUES (:recipe_id, :i, 1)"
+            db.session.execute(sql, {"recipe_id":recipe_id, "i":i})
+
+    sql = "SELECT tag_id, visible FROM recipetags WHERE recipe_id=:recipe_id"
+    result = db.session.execute(sql, {"recipe_id":recipe_id}).fetchall()
+    old_tags = [t[0] for t in result if t[1] == 1]
+    removed_tags = [t[0] for t in result if t[1] == 0]
+    new_tags = [int(t) for t in tags if t != ""]
+    for t in new_tags:
+        if t in removed_tags:
+            sql = "UPDATE recipetags SET visible=1 WHERE recipe_id=:recipe_id AND tag_id=:t"
+            db.session.execute(sql, {"recipe_id":recipe_id, "t":t})
+        elif t not in old_tags[:]:
+            sql = "INSERT INTO recipetags (recipe_id, tag_id, visible) VALUES (:recipe_id, :t, 1)"
+            db.session.execute(sql, {"recipe_id":recipe_id, "t":t})
+        else:
+            old_tags.remove(t)
+    if old_tags:
+        for t in old_tags:
+            sql = "UPDATE recipetags SET visible=0 WHERE recipe_id=:recipe_id AND tag_id=:t"
+            db.session.execute(sql, {"recipe_id":recipe_id, "t":t})
+
+    db.session.commit()
+    return redirect(f"/recipe/{recipe_id}")
+
+@app.route("/grade-recipe", methods=["POST"])
+def grade_recipe():
+    csrf_check(request.form["csrf_token"])
+    recipe_id = request.form["recipe_id"]
+    grade = request.form["grade"]
+    sql = "INSERT INTO grades (recipe_id, grade, visible) VALUES (:recipe_id, :grade, 1)"
+    db.session.execute(sql, {"recipe_id":recipe_id, "grade":grade})
+    db.session.commit()
+    return redirect(f"recipe/{recipe_id}")
+
+@app.route("/add-comment", methods=["POST"])
+def add_comment():
+    csrf_check(request.form["csrf_token"])
+    recipe_id = request.form["recipe_id"]
+    sender_id = get_user_id()
+    comment = request.form["comment"]
+    if len(comment) == 0:
+        return render_template("error.html", error="Kommentti on tyhjä.")
+    if len(comment) > 1000:
+        return render_template("error.html", error="Kommentti on liian pitkä.")
+    sql = """INSERT INTO comments (recipe_id, sender_id, comment, sent_at, visible) 
+             VALUES (:recipe_id, :sender_id, :comment, NOW(), 1)"""
+    db.session.execute(sql, {"recipe_id":recipe_id, "sender_id":sender_id, "comment":comment})
+    db.session.commit()
+    return redirect(f"recipe/{recipe_id}")
+
+@app.route("/search")
+def search():
+    keyword = "%" + request.args["keyword"].lower() + "%"
+    sql = """SELECT DISTINCT R.id, R.title FROM recipes R, ingredients I WHERE R.id=I.recipe_id AND 
+             R.visible=1 AND (LOWER(R.title) LIKE :keyword OR LOWER(R.description) LIKE :keyword OR 
+             LOWER(R.instruction) LIKE :keyword OR LOWER(I.ingredient) LIKE :keyword)"""
+    results = db.session.execute(sql, {"keyword":keyword}).fetchall()
+    return render_template("result.html", results=results)
 
 @app.route("/recipe/<int:id>")
 def recipe(id):
@@ -70,7 +160,7 @@ def recipe(id):
     creator = db.session.execute(sql, {"creator_id":creator_id}).fetchone()[0]
     sql = "SELECT T.tag FROM tags T, recipetags R WHERE R.recipe_id=:id AND T.id=R.tag_id AND R.visible=1"
     tags = db.session.execute(sql, {"id":id}).fetchall()
-    sql = "SELECT ingredient FROM ingredients WHERE recipe_id=:id"
+    sql = "SELECT ingredient FROM ingredients WHERE recipe_id=:id AND visible=1"
     ingredients = db.session.execute(sql, {"id":id}).fetchall()
     sql = """SELECT U.username, C.comment, C.sent_at FROM users U, comments C 
              WHERE U.id=C.sender_id AND C.recipe_id=:id ORDER BY C.sent_at"""
@@ -80,6 +170,26 @@ def recipe(id):
     else:
         own_recipe = False
     return render_template("recipe.html", creator=creator, recipe=recipe, tags=tags, ingredients=ingredients, comments=comments, own_recipe=own_recipe)
+
+@app.route("/delete-recipe", methods=["POST"])
+def delete_recipe():
+    csrf_check(request.form["csrf_token"])
+    recipe_id = request.form["recipe_id"]
+    creator_id = request.form["creator_id"]
+    if get_user_id() != int(creator_id):
+        return render_template("error.html", error="Toiminto ei ole sallittu.")
+    sql = "UPDATE recipes SET visible=0 WHERE id=:recipe_id"
+    db.session.execute(sql, {"recipe_id":recipe_id})
+    sql = "UPDATE ingredients SET visible=0 WHERE recipe_id=:recipe_id"
+    db.session.execute(sql, {"recipe_id":recipe_id})
+    sql = "UPDATE recipetags SET visible=0 WHERE recipe_id=:recipe_id"
+    db.session.execute(sql, {"recipe_id":recipe_id})
+    sql = "UPDATE comments SET visible=0 WHERE recipe_id=:recipe_id"
+    db.session.execute(sql, {"recipe_id":recipe_id})
+    sql = "UPDATE grades SET visible=0 WHERE recipe_id=:recipe_id"
+    db.session.execute(sql, {"recipe_id":recipe_id})
+    db.session.commit()
+    return redirect("/")
 
 @app.route("/new-user")
 def new_user():
@@ -135,61 +245,6 @@ def logout():
     del session["username"]
     return redirect("/")
 
-@app.route("/add-comment", methods=["POST"])
-def add_comment():
-    csrf_check(request.form["csrf_token"])
-    recipe_id = request.form["recipe_id"]
-    sender_id = get_user_id()
-    comment = request.form["comment"]
-    if len(comment) == 0:
-        return render_template("error.html", error="Kommentti on tyhjä.")
-    if len(comment) > 1000:
-        return render_template("error.html", error="Kommentti on liian pitkä.")
-    sql = """INSERT INTO comments (recipe_id, sender_id, comment, sent_at, visible) 
-             VALUES (:recipe_id, :sender_id, :comment, NOW(), 1)"""
-    db.session.execute(sql, {"recipe_id":recipe_id, "sender_id":sender_id, "comment":comment})
-    db.session.commit()
-    return redirect(f"recipe/{recipe_id}")
-
-@app.route("/grade-recipe", methods=["POST"])
-def grade_recipe():
-    csrf_check(request.form["csrf_token"])
-    recipe_id = request.form["recipe_id"]
-    grade = request.form["grade"]
-    sql = "INSERT INTO grades (recipe_id, grade, visible) VALUES (:recipe_id, :grade, 1)"
-    db.session.execute(sql, {"recipe_id":recipe_id, "grade":grade})
-    db.session.commit()
-    return redirect(f"recipe/{recipe_id}")
-
-@app.route("/search")
-def search():
-    keyword = "%" + request.args["keyword"].lower() + "%"
-    sql = """SELECT DISTINCT R.id, R.title FROM recipes R, ingredients I WHERE R.id=I.recipe_id AND 
-             R.visible=1 AND (LOWER(R.title) LIKE :keyword OR LOWER(R.description) LIKE :keyword OR 
-             LOWER(R.instruction) LIKE :keyword OR LOWER(I.ingredient) LIKE :keyword)"""
-    results = db.session.execute(sql, {"keyword":keyword}).fetchall()
-    return render_template("result.html", results=results)
-
-@app.route("/delete-recipe", methods=["POST"])
-def delete_recipe():
-    csrf_check(request.form["csrf_token"])
-    recipe_id = request.form["recipe_id"]
-    creator_id = request.form["creator_id"]
-    if get_user_id() != int(creator_id):
-        return render_template("error.html", error="Toiminto ei ole sallittu.")
-    sql = "UPDATE recipes SET visible=0 WHERE id=:recipe_id"
-    db.session.execute(sql, {"recipe_id":recipe_id})
-    sql = "UPDATE ingredients SET visible=0 WHERE recipe_id=:recipe_id"
-    db.session.execute(sql, {"recipe_id":recipe_id})
-    sql = "UPDATE recipetags SET visible=0 WHERE recipe_id=:recipe_id"
-    db.session.execute(sql, {"recipe_id":recipe_id})
-    sql = "UPDATE comments SET visible=0 WHERE recipe_id=:recipe_id"
-    db.session.execute(sql, {"recipe_id":recipe_id})
-    sql = "UPDATE grades SET visible=0 WHERE recipe_id=:recipe_id"
-    db.session.execute(sql, {"recipe_id":recipe_id})
-    db.session.commit()
-    return redirect("/")
-
 def get_user_id():
     try:
         username = session["username"]
@@ -201,3 +256,16 @@ def get_user_id():
 def csrf_check(token):
     if session["csrf_token"] != token:
         return render_template("error.html", error="Toiminto ei ole sallittu.")
+
+def check_recipe_inputs(title, description, instruction, ingredients):
+    if len(title) == 0:
+        return render_template("error.html", error="Reseptillä ei ole otsikkoa.")
+    if len(title) > 50:
+        return render_template("error.html", error="Otsikon maksimipituus on 50 merkkiä.")
+    if len(description) > 200:
+        return render_template("error.html", error="Kuvauksen maksimipituus on 200 merkkiä.")
+    if len(instruction) > 2000:
+        return render_template("error.html", error="Ohjeen maksimipituus on 2000 merkkiä.")
+    for i in ingredients:
+        if len(i) > 100:
+            return render_template("error.html", error="Ainesosan maksimipituus on 100 merkkiä.")
